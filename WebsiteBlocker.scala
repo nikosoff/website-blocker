9d95ec29a2185ad65561dbdd7dd8da63fdb7d38b
unknown <nikos_nick@outlook.com>
2018-03-07 17:05:43 +0200

    Add changes to WebsiteBlocker.scala file

--- WebsiteBlocker.scala
+++ WebsiteBlocker.scala
@@ -13,9 +13,26 @@ object WebsiteBlocker {
     
         val filename = "C:\\Windows\\System32\\drivers\\etc\\hosts"
         val redirect = "127.0.0.1"
-        val websiteList = List("www.facebook.com")
-        val lowerHourBound = 8
-        val upperHourBound = 20
+        val websiteList = Source.fromFile("block_websites.txt").getLines.toList  
+        var lowerHourBound = -1
+        var upperHourBound = -1
+        
+        if (args.length == 2) {
+            try {
+                lowerHourBound = args(0).toInt
+                upperHourBound = args(1).toInt
+            } catch {
+                case nfe: NumberFormatException => { 
+                    println("The arguments must be integers")
+                    System.exit(0)
+                }
+            }
+        }
+        else {
+            println("You should give two arguments that specify the time interval for which websites must be blocked")
+            println("For example: scala WebsiteBlocker.scala 11 16")
+            System.exit(0)
+        }
     
         while (true) {
 
@@ -36,7 +53,10 @@ object WebsiteBlocker {
                     outputFile.close()
             
                 } catch {
-                    case fnf: FileNotFoundException => println("The file \"" + filename +"\" does not exist")
+                    case fnf: FileNotFoundException => {
+                            println("The file \"" + filename +"\" does not exist")
+                            System.exit(0)
+                        }
                 }
             
             }
@@ -52,7 +72,10 @@ object WebsiteBlocker {
                     outputFile.close()
                 
                 } catch {
-                    case fnf: FileNotFoundException => println("The file \"" + filename + "\" does not exist")
+                    case fnf: FileNotFoundException => {
+                            println("The file \"" + filename + "\" does not exist")
+                            System.exit(0)
+                        }
                 }
             }
             
